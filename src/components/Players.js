91fc90d61441db7a1c7d4a92fd98d727758c5620
Matthias DELARETTE <m.delarette@foussier.fr>
2019-03-20 09:17:31 +0100

    Cleaning errors after clean !

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import useLocalStorage from "../hooks/use-local-storage";
 
 import Player from "./Player";
 
 const Players = props => {
-  const [name, setName] = useLocalStorage("name", "Bob");
-
   return (
     <React.Fragment>
-      {/* <input type="text" placeholder="Enter your name" value={name} onChange={e => setName(e.target.value)} /> */}
-
       <div>
         {props.players.map((player, index) => (
           <Player key={player.id} player={player} />
@@ -25,11 +20,6 @@ const mapStateToProps = (state, ownProps) => {
     players: state.bgg.players
   };
 };
-
-// const mapDispatchToProps = dispatch => ({
-//   fetchReclamation: id => dispatch(fetchReclamation(id)),
-//   initDetailsReclamation: () => dispatch(initDetailsReclamation())
-// });
 
 export default connect(
   mapStateToProps,
