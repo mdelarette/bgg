93b5a865d88edb34b8fc105f5d29b1acd67e8cc1
Matthias Delarette <mdelarette@foussier.fr>
2019-05-23 14:56:13 +0200

    Updates

---
+++ precache-manifest.544c245e7a0e72b3bb6ec3db9b4b2c0b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74bbabdeb7ef7f4b3e3f66724da8ded",
+    "url": "/bgg/index.html"
+  },
+  {
+    "revision": "86af95faf9d8a443e8ea",
+    "url": "/bgg/static/js/2.1a1183c6.chunk.js"
+  },
+  {
+    "revision": "2631dd78b93f76619693",
+    "url": "/bgg/static/js/main.a1dc4a80.chunk.js"
+  },
+  {
+    "revision": "ba0ff246e1ef83f7a7f3",
+    "url": "/bgg/static/js/runtime~main.630e163c.js"
+  }
+]);
